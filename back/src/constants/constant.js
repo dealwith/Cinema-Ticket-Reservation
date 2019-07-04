a3e99cc8c923ad3996ec262d8574bea213b9d7ca
Gleb <krishin@inbox.ru>
2019-07-05 01:27:48 +0300

    123

--- back/src/constants/constant.js
+++ back/src/constants/constant.js
@@ -1,11 +1,18 @@
 const PROFILE = '/profile';
 const LOGIN = '/login';
 const SIGNUP = '/signup';
-const MOVIES = '/movies'
+const MOVIES = '/movies';
+const SEARCH = '/search';
+const CITIES = '/cities';
+const CINEMAS = '/cinemas';
+
 
 module.exports = {
     SIGNUP,
     LOGIN,
     PROFILE,
     MOVIES,
+    SEARCH,
+    CITIES,
+    CINEMAS
 }
--- back/src/routes/routers.js
+++ back/src/routes/routers.js
@@ -1,9 +1,16 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const { login, signup } = require('../services/auth');
 const { search } = require('../services/search');
-const { PROFILE, LOGIN, SIGNUP, MOVIES } = require('../constants/constant');
+const   {   PROFILE, 
+            LOGIN, 
+            SIGNUP, 
+            MOVIES, 
+            SEARCH, 
+            CINEMAS, 
+            CITIES
+        } = require('../constants/constant');
 const { Movie, City, Cinema, CinemaShedule } = require('../sequelize');
 const { Op } = require('../sequelize');
 
@@ -29,7 +36,7 @@ router.get('/', async (req, res) => {
     res.json(suggestions)
 })
 
-router.post('/search', async (req, res) => {
+router.post(SEARCH, async (req, res) => {
     const response = await search(req.body);
     res.json(response);
 })
@@ -38,19 +45,47 @@ router.get(MOVIES, async (req, res) => {
     await Movie.findAll().then(movies => res.json(movies))
 })
 
-router.get(MOVIES + '/:id', async (req, res) => {
+router.get(MOVIES + '/:id', async (req, res, next) => {
         let id = req.params.id;
         const response = await Movie.findByPk(id)
         res.send(response)
 })
 
-router.get('/cities', async (req,res) => {
+router.get(CITIES, async (req,res) => {
     await City.findAll().then(cities => res.json(cities))
 })
 
-router.get('/cinemas', async (req, res) => {
+router.get(CINEMAS, async (req, res) => {
     await Cinema.findAll().then(cinemas => res.json(cinemas)) 
 })
+
+router.get(MOVIES, (req, res) => {
+
+    // let reqCityId = req.query.citySelect;
+    let city = req.params.city;
+    // let cinema = req.query.cinema;
+    // let movie = req.query.movie;
+
+    // res.send(city, cinema, movie)
+    // res.json(city, cinema, movie)
+    // await res.json(city, cinema. movie)
+    console.log(city)
+    res.send(city)
+    // await City.findAll({
+    //     include: [{
+    //         model: Cinema,
+    //         where: {
+    //             cityId: reqCityId
+    //         },
+    //         include: [{
+    //             model: CinemaShedule,
+    //         }]
+    //     }]
+    // })
+    //     .then(shedules => res.json(shedules))
+})
+
+
 
 // cinemaShedule
 router.get('/city-select', async (req, res) => {
@@ -67,31 +102,6 @@ router.get('/city-select', async (req, res) => {
         }]
     })
     .then(shedules => res.json(shedules))
-})
-
-router.get(MOVIES + '/filter?', async (req, res) => {
-    // let reqCityId = req.query.citySelect;
-
-    let city = req.query.city;
-    let cinema = req.query.cinema;
-    let movie = req.query.movie;
-
-    res.send(city, cinema, movie)
-    // console.log(cinema)
-    // console.log(movie)
-    
-    // await City.findAll({
-    //     include: [{
-    //         model: Cinema,
-    //         where: {
-    //             cityId: reqCityId
-    //         },
-    //         include: [{
-    //             model: CinemaShedule,
-    //         }]
-    //     }]
-    // })
-    //     .then(shedules => res.json(shedules))
 })
 
 router.get('/cinema-select', async (req, res) => {
