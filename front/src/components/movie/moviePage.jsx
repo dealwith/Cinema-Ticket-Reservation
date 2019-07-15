8b8cc4c2349599af3f59c79b3fdcb55b0915ebee
Hleb Kryshyn <Hleb.Kryshyn@itechart-group.com>
2019-07-15 14:00:54 +0300

    add single request for movie page

--- front/src/components/movie/moviePage.jsx
+++ front/src/components/movie/moviePage.jsx
@@ -16,18 +16,6 @@ class MoviePage extends React.Component {
         };
     }
 
-    getMovie = () => {
-        const { match: {params} } = this.props;
-
-        axios
-            .get(`http://localhost:3000/movies/${params.movieId}`)
-            .then(({ data: movie}) => {
-                this.setState({movie})
-            })
-            .catch(err => err.data)
-    }
-
-
     handleCityChange = e => {
         const val = e.value;
         this.setState({ citySelect: val })
@@ -46,72 +34,73 @@ class MoviePage extends React.Component {
         console.log(e);
     }
 
-    
-
     getShedules = () => {
         const state = this.state;
         const CITY_SELECT = state.citySelect;
         const CINEMA_SELECT = state.cinemaSelect
         const MOVIE_SELECT = state.movieSelect;
+        const { match: {params} } = this.props;
         
         axios
-            .get(`http://localhost:3000/movies/:id?city=${CITY_SELECT}&cinema=${CINEMA_SELECT}&movie=${MOVIE_SELECT}`)
-            .then(result => console.log(result))
+            .get(`http://localhost:3000/movies/${params.movieId}?city=${CITY_SELECT}&cinema=${CINEMA_SELECT}&movie=${MOVIE_SELECT}`)
+            .then( ({data: movieOnPage})  => this.setState({
+                movie: movieOnPage
+            }))
+            .catch(err => console.log(err))
     }
 
     componentDidMount() {
-        this.getMovie();
         this.getShedules();
     }
 
-    componentDidUpdate(prevProps, prevState) {
-        this.getShedules();
-    }
-    
-    
     render() {
-
         const state = this.state;
-        const movie = state.movie;
+        const movie = state.movie['movieOnPage'] 
         const CITY_SELECT = state.citySelect;
         const CINEMA_SELECT = state.cinemaSelect
         const MOVIE_SELECT = state.movieSelect;
-        
-        return (
-            <Container>
-                <div className="movie-filter mb-3 d-flex justify-content-around">
-                    <CitySelect name='citySelect' 
-                                value={ CITY_SELECT } 
-                                handleChange={ this.handleCityChange } 
-                    />
-                    <CinemaSelect   name='cinemaSelect' 
-                                    value={ CINEMA_SELECT } 
-                                    handleChange={this.handleCinemaChange} 
-                    />
-                    <MovieSelect    name='movieSelect' 
-                                    value={ MOVIE_SELECT }      
-                                    handleChange={this.handleMovieChange} 
-                    />
-                </div>
-                <div className='card mb-4' style={{height: 'auto'}}>
-                    <div className="row no-gutters">
-                        <div className="col md-5">
-                            <img src={movie.imgUrl} alt="" className="card-img rounded" style={{maxHeight: 500, objectFit: 'contain'}}/>
-                        </div>
-                        <div className="col md-8">
-                            <div className="card-body">
-                                <h2 className="card-tittle">{movie.name}</h2>
-                                <p className="card-text">{movie.description}</p>
+        if(!movie){
+            return <h1>loading</h1>
+        }else{
+            return (
+                <Container>
+                    <div className="movie-filter mb-3 d-flex justify-content-around">
+                        <CitySelect name='citySelect' 
+                                    value={ CITY_SELECT } 
+                                    handleChange={ this.handleCityChange } 
+                        />
+                        <CinemaSelect   name='cinemaSelect' 
+                                        value={ CINEMA_SELECT } 
+                                        handleChange={this.handleCinemaChange} 
+                        />
+                        <MovieSelect    name='movieSelect' 
+                                        value={ MOVIE_SELECT }      
+                                        handleChange={this.handleMovieChange} 
+                        />
+                    </div>
+                    <div className='card mb-4' style={{height: 'auto'}}>
+                        <div className="row no-gutters">
+                            <div className="col md-5">
+                                <img src={movie.imgUrl} alt="" className="card-img rounded" style={{maxHeight: 500, objectFit: 'contain'}}/>
+                            </div>
+                            <div className="col md-8">
+                                <div className="card-body">
+                                    <h2 className="card-tittle">{movie.name}</h2>
+                                    <p className="card-text">{movie.description}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="cinema-schedules">
-                    <Schedule />
-                </div>
-            </Container>
-        )
+                    <div className="cinema-schedules">
+                        <Schedule />
+                    </div>
+                </Container>
+            )
+        }
+        
     }
 }
 
 export default MoviePage
+
+
